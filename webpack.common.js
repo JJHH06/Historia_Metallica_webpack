eaf151ee6f10ff66bf0c08454d22c9a8301ce116
Jose Hurtarte <JJHH121106@gmail.com>
2021-03-03 16:40:34 -0600

    html loader and file loader plugin configuration

---
+++ webpack.common.js
@@ -0,0 +1,34 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+
+module.exports = {
+    entry:"./src/index.js",
+    
+    plugins: [new HtmlWebpackPlugin({
+        template: "./src/menu/menu.html"
+    }
+    )],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.html$/,
+                use: ["html-loader"]
+            },
+            {
+                test: /\.(svg|png|jpg|jpeg|gif)$/,
+                use:{
+                    loader: "file-loader",
+                    options: {
+                        name: "[name].[hash].[ext]",
+                        outputPath: "assets"
+                    }
+                }
+            }
+
+        ]
+    }
+};
