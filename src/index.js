c7ac9626d591d6568e682f9011300ae447c2372f
Jose Hurtarte <JJHH121106@gmail.com>
2021-03-04 01:45:51 -0600

    colocación imagenes menu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-alert("Hello xdxdxd");
+//alert("Hello xdxdxd");
 import "./menu/carousel.scss"
 import "bootstrap/dist/js/bootstrap"
+import "./menu/menu"
---
+++ src/menu/menu.js
@@ -0,0 +1,14 @@
+import portada1 from '../assets/wallpaper4.jpg';
+import portada2 from '../assets/wallpaper2.jpg';
+import portada3 from '../assets/wallpaper3.jpg';
+
+//<img class="bd-placeholder-img" id="portada1" width="100%" height="100%"  aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></img>
+
+var imagen1 = document.getElementById("portada1");
+imagen1.src = portada2;
+
+var imagen1 = document.getElementById("portada2");
+imagen1.src = portada1;
+
+var imagen1 = document.getElementById("portada3");
+imagen1.src = portada3;
