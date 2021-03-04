7e68b3bf0bd753758bc65698021b1ff4025cb01d
Jose Hurtarte <JJHH121106@gmail.com>
2021-03-04 02:31:22 -0600

    Estilo del menu

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//alert("Hello xdxdxd");
+alert("Hello xdxdxd");
 import "./menu/carousel.scss"
 import "bootstrap/dist/js/bootstrap"
 import "./menu/menu"
--- src/menu/menu.js
+++ src/menu/menu.js
@@ -1,14 +1,32 @@
+//imagenes de la portada
 import portada1 from '../assets/wallpaper4.jpg';
 import portada2 from '../assets/wallpaper2.jpg';
 import portada3 from '../assets/wallpaper3.jpg';
+//imagenes de la historia
+import historia1 from '../assets/portada-historia1.jpg'
+import historia2 from '../assets/portada-historia2.jpg'
+import historia3 from '../assets/portada-historia3.jpg'
+import historia4 from '../assets/portada-historia4.png'
+import historia5 from '../assets/portada-historia5.jpg'
+import historia6 from '../assets/portada-historia6.jpg'
+import historia7 from '../assets/portada-historia7.jpg'
 
-//<img class="bd-placeholder-img" id="portada1" width="100%" height="100%"  aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false"><rect width="100%" height="100%" fill="#777"/></img>
+
 
 var imagen1 = document.getElementById("portada1");
 imagen1.src = portada2;
-
 var imagen1 = document.getElementById("portada2");
 imagen1.src = portada1;
-
 var imagen1 = document.getElementById("portada3");
 imagen1.src = portada3;
+
+
+//menu-historia1
+var menuHistoria1 = document.getElementById("menu-historia1");
+menuHistoria1.src = historia1;
+
+var menuHistoria2 = document.getElementById("menu-historia2");
+menuHistoria2.src = historia2;
+
+var menuHistoria3 = document.getElementById("menu-historia3");
+menuHistoria3.src = historia3;
